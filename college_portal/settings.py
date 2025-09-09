9e42c510d38843a3029ed8554c217cb7f0e209b3
DevRohan33 <parveagr@gmail.com>
2025-09-09 14:03:49 +0530

    switch db

--- college_portal/settings.py
+++ college_portal/settings.py
@@ -127,38 +127,38 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 #sqlit3
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # if running on Render (Render sets DATABASE_URL automatically)
-# DATABASE_URL = config("DATABASE_URL", default="")
+DATABASE_URL = config("DATABASE_URL", default="")
 
-# if DATABASE_URL:
-#     # Running on Render → SSL required
-#     DATABASES = {
-#         "default": dj_database_url.config(
-#             default=DATABASE_URL,
-#             conn_max_age=600,
-#             ssl_require=True
-#         )
-#     }
-# else:
-#     # Running locally → use Postgres without SSL
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             "NAME": "clg_portal",
-#             "USER": "rohan",
-#             "PASSWORD": "Rohan@330",
-#             "HOST": "localhost",
-#             "PORT": "5432",
-#         }
-#     }
+if DATABASE_URL:
+    # Running on Render → SSL required
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Running locally → use Postgres without SSL
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "clg_portal",
+            "USER": "rohan",
+            "PASSWORD": "Rohan@330",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+    }
 
 
 
